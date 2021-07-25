28582cfd29ba9e2a7ad42fcccd5eda1df16f02a2
Jiyon Lee <jiyonlee.d@gmail.com>
2021-07-25 22:02:19 +0900

    Modify: Nav bar Layout

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,83 +1,49 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { flexSet } from '../../styles/Variable';
 import styled from 'styled-components/macro';
 import TopBanner from './TopBanner';
+import { flexSet } from '../../styles/Variable';
+import { mediaQuery } from '../../styles/Variable';
 
-function Nav() {
+const COMMUNITY_CATEGORY_LIST = [
+  { menuName: '홈', path: '/' },
+  { menuName: '사진', path: '/photo' },
+  { menuName: '집들이', path: '/houseparty' },
+  { menuName: '노하우', path: '/knowhow' },
+  { menuName: '전문가집들이', path: '/experthouseparty' },
+  { menuName: '셀프가이드', path: '/selfguide' },
+  { menuName: '질문과답변', path: '/questionandanswer' },
+  { menuName: '이벤트', path: '/event' },
+];
+
+function Nav({ theme }) {
   const location = useLocation();
   const isLoggedIn = localStorage.getItem('access_token');
-
-  const COMMUNITY_CATEGORY_LIST = [
-    { menuName: '홈', path: '/' },
-    { menuName: '사진', path: '/photo' },
-    { menuName: '집들이', path: '/houseparty' },
-    { menuName: '노하우', path: '/knowhow' },
-    { menuName: '전문가집들이', path: '/experthouseparty' },
-    { menuName: '셀프가이드', path: '/selfguide' },
-    { menuName: '질문과답변', path: '/questionandanswer' },
-    { menuName: '이벤트', path: '/event' },
-  ];
 
   return (
     <>
       <TopBanner />
-      <MainNav>
-        <div>은행의집</div>
-        <nav>
-          <div>커뮤니티</div>
-          <div>스토어</div>
-          <div>인테리어시공</div>
-        </nav>
-        <div>오늘의집 통합검색</div>
-        <div>
-          <i class="fas fa-shopping-cart"></i>
-        </div>
-        <div>로그인</div>
-        <div>회원가입</div>
-        <button>글쓰기</button>
-      </MainNav>
-      {/* <MainNavContainer>
-        <MainNavContainerInnerWrapper>
-          <GoToMainPageLink to="/">은행의집</GoToMainPageLink>
-          <LeftMenuCategoryWrapper>
-            <LeftMenuCategorySelected>커뮤니티</LeftMenuCategorySelected>
-            <LeftMenuCategoryUnselected>스토어</LeftMenuCategoryUnselected>
-            <LeftMenuCategoryUnselected>
-              인테리어시공
-            </LeftMenuCategoryUnselected>
-          </LeftMenuCategoryWrapper>
-          <RightMenuCategoryWrapper>
-            <SearchBoxWrapper>
-              <FaSearchIcon className="fas fa-search" />
-              <SearchBoxInput
-                aria-label="커뮤니티, 스토어 및 인테리어시공 검색"
-                placeholder="은행의집 통합검색"
-                type="search"
-              />
-              <SearchBoxButton>
-                <FaCameraIcon className="fas fa-camera" />
-              </SearchBoxButton>
-            </SearchBoxWrapper>
-            <CartIconWrapper>
-              <FaShoppingCartIcon className="fas fa-shopping-cart" />
-            </CartIconWrapper>
-            <LoginAndSignUpWrapper>
-              <GoToLoginPageLink to="/login">
-                {isLoggedIn ? '로그아웃' : '로그인'}
-              </GoToLoginPageLink>
-              <GoToSignUpPageLink to="/signup">회원가입</GoToSignUpPageLink>
-            </LoginAndSignUpWrapper>
-            <WritingMenuWrapper>
-              <WritingMenuButton>
-                <GoToWritingPageLink to="/write">글쓰기</GoToWritingPageLink>
-                <FaChevronDownIcon className="fas fa-chevron-down" />
-              </WritingMenuButton>
-            </WritingMenuWrapper>
-          </RightMenuCategoryWrapper>
-        </MainNavContainerInnerWrapper>
-      </MainNavContainer> */}
-
+      <TopNav>
+        <NavLogo to="/">은행의집</NavLogo>
+        <Gnb>
+          <GnbMenu current>커뮤니티</GnbMenu>
+          <GnbMenu>스토어</GnbMenu>
+          <GnbMenu>인테리어시공</GnbMenu>
+        </Gnb>
+        <NavSearch>
+          <NavSearchText placeholder="은행의집 통합검색" />
+          <MagnifyingIcon className="fas fa-search" />
+          <CameraIcon className="fas fa-camera" />
+        </NavSearch>
+        <CartAndLogin>
+          <CartIcon className="fas fa-shopping-cart" />
+          <LoginIcon>
+            <NavLogin>로그인</NavLogin>
+            <NavLogin>회원가입</NavLogin>
+          </LoginIcon>
+        </CartAndLogin>
+        <NavWriteBtn>글쓰기</NavWriteBtn>
+      </TopNav>
       <BottomNavContainer>
         <BottomNavContainerInnerWrapper>
           <CommunityMenuWrapper>
@@ -103,198 +69,135 @@ function Nav() {
   );
 }
 
-// const MainNavContainer = styled.div`
-//   position: sticky;
-//   top: 0px;
-//   right: 0px;
-//   left: 0px;
-//   height: 80px;
-//   background-color: #ffffff;
-//   border-bottom: 1px solid ${({ theme }) => theme.borderLine};
-//   z-index: 3;
-// `;
+const TopNav = styled.div`
+  display: flex;
+  align-items: center;
+  /* justify-content: space-between; */
+  flex: 1 0 auto;
+  padding: 10px 60px;
+  height: 80px;
+  margin: 0 auto;
+  max-width: 1256px;
 
-// const MainNavContainerInnerWrapper = styled.div`
-//   ${flexSet()}
-//   max-width: 1256px;
-//   height: 80px;
-//   margin: 0 auto;
-//   padding: 10px 60px;
-// `;
+  @media only screen and (max-width: ${mediaQuery.BREAK_POINT_TABLET}px) {
+    padding: 10px 40px;
+  }
+`;
 
-// const GoToMainPageLink = styled(Link)`
-//   width: 15%;
-//   font-family: '잘풀리는오늘';
-//   font-size: 26px;
-//   color: inherit;
-//   text-decoration: none;
+const NavLogo = styled(Link)`
+  position: static;
+  margin-right: 10px;
+  font-size: 25px;
+  font-family: '잘풀리는오늘';
+  transform: none;
+`;
 
-//   &:hover {
-//     opacity: 0.7;
-//     cursor: pointer;
-//   }
-// `;
+const Gnb = styled.nav`
+  display: flex;
+  flex: 1 0 auto;
+`;
 
-// const LeftMenuCategoryWrapper = styled.ul`
-//   display: flex;
-//   width: 35%;
-//   color: ${({ theme }) => theme.fontMainBlack};
-//   font-size: 18px;
-//   font-weight: 700;
-// `;
-
-// const LeftMenuCategorySelected = styled.li`
-//   margin-right: 15px;
-//   color: ${({ theme }) => theme.mainBlue};
-
-//   &:hover {
-//     cursor: pointer;
-//   }
-// `;
-
-// const LeftMenuCategoryUnselected = styled(
-//   LeftMenuCategorySelected.withComponent('li')
-// )`
-//   color: ${({ theme }) => theme.fontMainBlack};
-// `;
-
-// const RightMenuCategoryWrapper = styled.div`
-//   ${flexSet('flex-end')};
-//   width: 60%;
-// `;
-
-// const SearchBoxWrapper = styled.div`
-//   ${flexSet('start')};
-//   position: relative;
-//   width: 55%;
-//   margin-right: 8px;
-
-//   &:hover {
-//     cursor: text;
-//   }
-// `;
-
-// const FaSearchIcon = styled.i`
-//   position: absolute;
-//   transform: translateY(5%);
-//   left: 7px;
-//   color: ${({ theme }) => theme.fontMainBlack};
-// `;
-
-// const SearchBoxInput = styled.input`
-//   width: 100%;
-//   padding: 7px 30px;
-//   border: 1px solid #dbdbdb;
-//   border-radius: 5px;
-
-//   &:hover {
-//     cursor: text;
-//   }
-// `;
-
-// const SearchBoxButton = styled.button`
-//   position: absolute;
-//   top: 0px;
-//   transform: translateY(30%);
-//   right: 5px;
-//   font-size: 20px;
-//   background-color: transparent;
-//   border: 0;
-
-//   &:hover {
-//     cursor: pointer;
-//     color: ${({ theme }) => theme.mainBlue};
-//   }
-// `;
-
-// const FaCameraIcon = styled.i`
-//   position: absolute;
-//   top: 0px;
-//   transform: translateY(30%);
-//   right: 5px;
-//   color: ${({ theme }) => theme.fontMainBlack};
-//   font-size: 20px;
-// `;
-
-// const CartIconWrapper = styled.div`
-//   width: 5%;
-//   color: ${({ theme }) => theme.fontMainBlack};
-
-//   &:hover {
-//     cursor: pointer;
-//     color: ${({ theme }) => theme.mainBlue};
-//   }
-// `;
-
-// const FaShoppingCartIcon = styled.i`
-//   ${flexSet('center', 'start')};
-// `;
-
-// const LoginAndSignUpWrapper = styled.div`
-//   ${flexSet('space-between')};
-//   width: 25%;
-//   margin: 0px 5px;
-// `;
-
-const GoToLoginPageLink = styled(Link)`
-  width: 43%;
-  border-right: 1px solid ${({ theme }) => theme.fontFilterGray};
-  color: ${({ theme }) => theme.fontMainBlack};
-  text-decoration: none;
-  text-align: center;
-  font-size: 16px;
+const GnbMenu = styled(Link)`
+  color: ${({ theme, current }) =>
+    current ? theme.mainBlue : theme.fontMainBlack};
+  font-size: 18px;
+  font-weight: 700;
+  padding: 14px 6px;
 
   &:hover {
-    cursor: pointer;
     color: ${({ theme }) => theme.mainBlue};
   }
 `;
 
-const GoToSignUpPageLink = styled(GoToLoginPageLink.withComponent(Link))`
-  width: 57%;
-  border-style: none;
-`;
-
-const WritingMenuWrapper = styled.div`
+const NavSearch = styled.div`
   position: relative;
-  width: 15%;
 `;
 
-const WritingMenuButton = styled.button`
-  width: 100%;
-  margin-right: 5px;
-  padding: 8px 25px 8px 12px;
-  background-color: transparent;
-  background-color: ${({ theme }) => theme.mainBlue};
-  border: 0;
-  border-radius: 5px;
+const NavSearchText = styled.input`
+  border: 1px solid ${({ theme }) => theme.inputGray};
+  border-radius: 4px;
+  padding: 8px 9px 10px 39px;
+  font-size: 15px;
 
-  &:hover {
-    opacity: 0.7;
-    cursor: pointer;
+  ::placeholder {
+    color: ${({ theme }) => theme.NavGrayInputPlaceholder};
   }
 `;
 
-const GoToWritingPageLink = styled(Link)`
-  font-weight: 700;
-  vertical-align: middle;
-  color: #ffffff;
-  text-decoration: none;
-  font-size: 14px;
-
-  &:hover {
-    opacity: 0.7;
-    cursor: pointer;
-  }
+const Icons = styled.i`
+  color: ${({ theme }) => theme.NavGrayIconBackground};
+  top: 11px;
+  width: 20px;
+  height: 20px;
 `;
 
-const FaChevronDownIcon = styled.i`
+const MagnifyingIcon = styled(Icons)`
   position: absolute;
-  top: 0px;
-  transform: translateY(80%);
+  left: 10px;
+`;
+
+const CartAndLogin = styled.div`
+  display: flex;
+  position: relative;
+  margin-right: 10px;
+`;
+
+const CameraIcon = styled(Icons)`
+  position: absolute;
   right: 10px;
-  color: #ffffff;
-  font-size: 13px;
+`;
+
+const CartIcon = styled(Icons)`
+  position: relative;
+  top: 1px;
+  margin: 0 9px;
+`;
+
+const LoginIcon = styled.div`
+  display: flex;
+  color: ${({ theme }) => theme.NavGrayFontColor};
+`;
+
+const NavLogin = styled.div`
+  position: relative;
+  margin: 0 9px;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 19px;
+
+  &:nth-child(1)::after {
+    content: '';
+    position: absolute;
+    top: 2px;
+    right: -25%;
+    width: 1px;
+    height: 15px;
+    background-color: ${({ theme }) => theme.NavGrayFontColor};
+  }
+`;
+
+const NavWriteBtn = styled.button`
+  position: relative;
+  padding: 8px 15px 10px 0;
+  width: 100px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.mainBlue};
+  color: #fff;
+  font-weight: 700;
+  font-size: 16px;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: 15px;
+    top: 50%;
+    width: 6px;
+    height: 6px;
+    border-right: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+    transform: rotate(45deg) translateY(-73%);
+  }
 `;
 
 const BottomNavContainer = styled.div`
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,11 @@
 const theme = {
   mainBlue: '#00c7f5',
   backgroundGrey: '#f5f5f5',
+
   // border color
   borderLine: '#ededed',
   inputGray: '#dbdbdb',
+
   // font color
   fontMainBlack: '#424242',
   fontNavBlack: '#474747',
@@ -11,6 +13,11 @@ const theme = {
   fontFilterGray: '#757575',
   fontGray: '#757575',
   placeholderGrey: '#bdbdbd',
+
+  // Main nav
+  NavGrayFontColor: '#757575',
+  NavGrayIconBackground: '#989898',
+  NavGrayInputPlaceholder: '#DCD9D6',
 };
 
 export default theme;
